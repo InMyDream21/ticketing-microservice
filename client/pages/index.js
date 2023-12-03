be4481440e5019f218803ca90a731c3025e64cf8
InMyDream21 <ahmed.nizhan@gmail.com>
2023-12-03 16:42:21 +0700

    Add Ticket Info and Order page.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,17 +1,41 @@
-import buildClient from "../api/build-client";
+import Link from "next/link";
 
-const landing = ({ currentUser }) => {
-  return currentUser ? (
-    <h1>You are signed in</h1>
-  ) : (
-    <h1>You are NOT signed in</h1>
+const landing = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            View
+          </Link>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
-landing.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = await client.get("/api/users/currentuser");
-  return data;
+landing.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get("/api/tickets");
+
+  return { tickets: data };
 };
 
 export default landing;
